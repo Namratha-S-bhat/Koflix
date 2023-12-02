28068ae21b263779cb2da1d793fdf70c40dbcef0
Namratha <bhatnamrathas@gmail.com>
2023-12-02 21:05:21 +0530

    Files added

---
+++ myScript.js
@@ -0,0 +1,18 @@
+function d1(){
+    window.open("https://www.viki.com/tv/34479c-fight-for-my-way")
+}
+function d2(){
+    window.open("https://www.viki.com/tv/37442c-the-goddess-of-revenge")
+}
+function d3(){
+    window.open("https://www.viki.com/tv/37374c-true-beauty?q=tru")
+}
+function d4(){
+    window.open("https://www.viki.com/tv/24782c-the-kings-face?q=the%20king")
+}
+function d5(){
+    window.open("https://www.viki.com/tv/39209c-the-secret-romantic-guesthouse")
+}
+function d6(){
+    window.open("https://www.viki.com/tv/35835c-whats-wrong-with-secretary-kim")
+}
